8ca56cbe47dba035a28aea067d6404a4f574034e
aymaneagle <aymanalwahid04@gmail.com>
2025-08-05 19:06:22 +0600

    add cards section with JSON-based dynamic rendering on HomePage fetched from public json

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+
 const HomePage = () => {
+    const[cardData, setCardData] = useState([]);
+    useEffect(() => {
+        fetch("/card-data.json")
+            .then((response) =>response.json())
+            .then((data) => setCardData(data));
+    }, []);
     return (
-        <div>
-            <h1>This is Home Page</h1>
-        </div>
+     <div className="grid grid-cols-3 gap-2">
+         {cardData.map((card)=>(
+             <div className="bg-gray-200 w-fit p-5 m-5 rounded-md">
+                 <h1 className="text-xl font-bold">{card.title}</h1>
+                 <p className="text-md text-yellow-600">{card.description}</p>
+                 <p className="text-gray-600 w-56">{card.body}</p>
+             </div>
+         ))}
+     </div>
     );
 };
 
